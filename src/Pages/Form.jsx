92b513503885e17dbbe9263799e9c603258ccd66
MoisesSantana <moisaant@gmail.com>
2021-03-28 10:31:32 -0300

    form class

--- src/Pages/Form.jsx
+++ src/Pages/Form.jsx
@@ -19,132 +19,205 @@ function Form() {
       <hr />
       <form>
         <div className="main-left">
-          <div className="row">
-            <label>
-            Qual sua cidade atual?
-              <input type="text" />
+          <div className="field row">
+            <label
+              className="label"
+            >
+              Qual sua cidade atual?
             </label>
+            <div className="control">
+              <input
+                className="input is-large"
+                type="text"
+                placeholder="Digite sua cidade"
+              />
+            </div>
           </div>
           <div className="row">
-            <div className="col">           
+            <div className="field col">
               <span>O que você prefere?</span> 
-              <label>
-                <input
-                  id="beach"
-                  value={ beachMountain }
-                  type="radio"
-                  name="beach-mountain"
-                  onClick={ (event) => setBeachMoutains(event.target.id) }
-                />
-                Praia
-              </label>
-              <label>
-                <input
-                  id="mountain"
-                  value={ beachMountain }
-                  type="radio"
-                  name="beach-mountain"
-                  onClick={ (event) => setBeachMoutains(event.target.id) }
-                />
-                Montanha
-              </label>
-              <label>
-                <input
-                  value="beach mountain"
-                  type="radio"
-                  name="beach-mountain"
-                  onClick={ (event) => setBeachMoutains(event.target.value) }
-                />
-                Ambos
-              </label>
+              <div className="control">
+                <label
+                  className="radio"
+                >
+                  <input
+                    id="beach"
+                    value={ beachMountain }
+                    type="radio"
+                    name="beach-mountain"
+                    onClick={ (event) => setBeachMoutains(event.target.id) }
+                  />
+                  Praia
+                </label>
+                <label
+                  className="radio"
+                >
+                  <input
+                    id="mountain"
+                    value={ beachMountain }
+                    type="radio"
+                    name="beach-mountain"
+                    onClick={ (event) => setBeachMoutains(event.target.id) }
+                  />
+                  Montanha
+                </label>
+                <label
+                  className="radio"
+                >
+                  <input
+                    value="beach mountain"
+                    type="radio"
+                    name="beach-mountain"
+                    onClick={ (event) => setBeachMoutains(event.target.value) }
+                  />
+                  Ambos
+                </label>
+              </div>
             </div>
-            <div className="col">
+            <div className="field col">
               <span>Qual a temperatura ideal para você?</span>
-              <label>
-                <input
-                  value="hot"
-                  type="radio"
-                  name="cold-hot-weather"
-                  // onClick={ (event) => setHot(event.target.value) }
-                />
-                Calor
-              </label>
-              <label>
-                <input
-                  type="radio"
-                  name="cold-hot-weather"
-                  onClick={ (event) => setBeachMoutains(event.target.value) }
-                />
-                Frio
-              </label>
-              <label>
-                <input type="radio" name="cold-hot-weather" />
-                Ambos
-              </label>        
+              <div className="control">
+                <label
+                  className="radio"
+                >
+                  <input
+                    value="hot"
+                    type="radio"
+                    name="cold-hot-weather"
+                    // onClick={ (event) => setHot(event.target.value) }
+                  />
+                  Calor
+                </label>
+                <label
+                  className="radio"
+                >
+                  <input
+                    type="radio"
+                    name="cold-hot-weather"
+                    onClick={ (event) => setBeachMoutains(event.target.value) }
+                  />
+                  Frio
+                </label>
+                <label
+                  className="radio"
+                >
+                  <input type="radio" name="cold-hot-weather" />
+                  Ambos
+                </label>
+              </div>
             </div>
           </div>
           <div className="row">
-            <div className="col">
-            <span>Quais serviços você utiliza atualmente?</span>
-              <label>
-                <input type="checkbox" name="services" />
-                Sites de reserva hospedagem online
-              </label>
-              <label>
-                <input type="checkbox" name="services" />
-                Compras online
-              </label>
-              <label>
-                <input type="checkbox" name="services" />
-                Transporte particular
-              </label>
-              <label>
-                <input type="checkbox" name="services" />
-                Aluguel de carros
-              </label>
-              <label>
-                <input type="checkbox" name="services" />
-                Delivery de comida
-              </label>
+            <div className="field col">
+              <span>Quais serviços você utiliza atualmente?</span>
+              <div className="control">
+                <label
+                  className="checkbox"
+                >
+                  <input type="checkbox" name="services" />
+                  Sites de reserva hospedagem online
+                </label>
+                <label
+                  className="checkbox"
+                >
+                  <input type="checkbox" name="services" />
+                  Compras online
+                </label>
+                <label
+                  className="checkbox"
+                >
+                  <input type="checkbox" name="services" />
+                  Transporte particular
+                </label>
+                <label
+                  className="checkbox"
+                >
+                  <input type="checkbox" name="services" />
+                  Aluguel de carros
+                </label>
+                <label
+                  className="checkbox"
+                >
+                  <input type="checkbox" name="services" />
+                  Delivery de comida
+                </label>
+              </div>
             </div>
           </div>
         </div>
         <div className="main-right">
-          <div className="row">
-            <label>
+          <div className="field row">
+            <label
+              className="label"
+            >
               Quais regiões você prefere?
-              <input type="text" />
             </label>
+            <div className="control">
+              <input
+                type="text"
+                placeholder="Escolha a região"
+              />
+            </div>
           </div>
           <div className="row">
-            <div className="col">
+            <div className="field col">
               <span>Qual localidade você prefere?</span>
-              <label>
-                <input type="radio" name="country-capital" />
-                Capital
-              </label>
-              <label>
-                <input type="radio" name="country-capital" />
-                Interior
-              </label>
-              <label>
-                <input type="radio" name="country-capital" />
-                Ambos
-              </label>
+              <div className="control">
+                <label
+                  className="radio"
+                >
+                  <input type="radio" name="country-capital" />
+                  Capital
+                </label>
+                <label
+                  className="radio"
+                >
+                  <input type="radio" name="country-capital" />
+                  Interior
+                </label>
+                <label
+                  className="radio"
+                >
+                  <input type="radio" name="country-capital" />
+                  Ambos
+                </label>
+              </div>
             </div>
-            <div className="col">
+            <div className="field col">
               <span>Seu próximo destino será:</span>
-              <label>
-                <input type="radio" name="national-international" />
-                Nacional
-              </label>
-              <label>
-                <input type="radio" name="national-international" />
-                Internacional
-              </label>
+              <div className="control">
+                <label
+                  className="radio"
+                >
+                  <input type="radio" name="national-international" />
+                  Nacional
+                </label>
+                <label
+                  className="radio"
+                >
+                  <input type="radio" name="national-international" />
+                  Internacional
+                </label>
+              </div>
             </div>
           </div>
-          <div className="row">
+          <div className="field row">
+            <label label="label">Mês de previsão de ida</label>
+            <div className="control">
+              <input
+                type="text"
+                className="input is-medium"
+                placeholder="Mês"
+              />
+            </div>
+            <label label="label">Ano</label>
+            <div className="control">
+              <input
+                type="text"
+                className="input is-medium"
+                placeholder="Ano"
+              />
+            </div>
             <button type="submit">
               Enviar
             </button>
